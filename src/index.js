a7478c670c08cc2e49384f35dc7f1ccb990c5c81
einarparker <76267622+einarparker@users.noreply.github.com>
2022-04-18 16:28:15 +0100

    more of ex

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <>
-      <h1>Created by {name}</h1>
+      <h1 className="heading">Created by {name}</h1>
       <p> @Copyright {year}</p>
     </>
   </React.StrictMode>,
